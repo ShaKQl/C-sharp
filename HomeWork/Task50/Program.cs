8bd6c041668a9dc342010107361e229b0a4c9df6
ShaKQl <halimovsvhzod@gmail.com>
2023-09-22 21:29:04 +0500

    H/w completed 2

--- HomeWork/Task50/Program.cs
+++ HomeWork/Task50/Program.cs
@@ -20,12 +20,12 @@ void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write("| ");
+     
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i, j],6}");
+            Console.Write($"{matrix[i, j]}");
         }
-        Console.WriteLine(" |");
+// 1	4	7	10	13
     }
 }
 
@@ -65,3 +65,14 @@ PrintFinalResult(matrix);
 
 
 
+ 
+
+
+
+
+
+
+
+
+
+       
